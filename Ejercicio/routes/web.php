4747ec6b1024a3437b2d6c810e0348a141c63969
JenniD5 <elizabethduran0105@gmail.com>
2021-01-26 10:30:12 -0700

    ejercicio 26-1

--- Ejercicio/routes/web.php
+++ Ejercicio/routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('nota/{id}/detalles', function($id){
+    return [
+        'Detalles del id:' .$id => [
+            'nombre',
+            'fecha',
+            'contenido',
+        ]
+        ];
+});
+
+
+Route::get('nota/crear', function(){
+    return 'Crear una nueva nota';
+});
+
+Route:: get('nota/listar', function(){
+    return 'aqui se listaran las notas';
+});
+
+Route :: get ('nota/editar', function(){
+    return 'aqui se editaran las notas';
+});
+Route:: get('nota/{id}', function($id){
+    return 'detalles de la nota con id:' .$id;
+});
