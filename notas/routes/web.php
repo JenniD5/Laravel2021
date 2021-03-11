2c8293d20463f03d901e013210c7e0b673a6e2cd
JenniD5 <elizabethduran0105@gmail.com>
2021-03-11 14:15:02 -0700

    controllers/11-03-2021

--- notas/routes/web.php
+++ notas/routes/web.php
@@ -19,27 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route :: get('notas', function() {
-    $notas = Notas::all();//DB::table('notas')->get();
-        
-    
-    return view ('notas', ['notas'=> $notas]);
-})->name('notas.index');
+Route :: get('notas','App\Http\Controllers\NotasController@index')->name('notas.index');
 
-Route::get('agregar', function(){
-    return view('agregar');
-});
+Route::get('agregar','App\Http\Controllers\NotasController@agregar' );
 
-Route::post('crear', function(Request $request){
-    Notas::create([
-        'titulo' => $request->input('title'),
-        'contenido' => $request->input('content'),
-    ]);
-        return redirect('/notas');
-})->name('notas.store');
+Route::post('crear', 'App\Http\Controllers\NotasController@crear')->name('notas.store');
 
-Route::get('notas/{id}/editar', function ($id){
-    $notas = Notas::find($id);//DB::table('notas') ->where('id', $id) ->first();
-        return view('editar', ['notas' => $notas]);
-        #return 'Aqui se van a editar las notas' .$id;
-})->name('notas.edit');
+Route::get('notas/{id}/editar', 'App\Http\Controllers\NotasController@editar')->name('notas.edit');
